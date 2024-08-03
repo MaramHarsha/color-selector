7d7fba90e2d11965dcf2b054f9a6ef5b6660da58
MaramSaiHarsha <smaram@loginsoft.com>
2024-08-04 05:18:43 +0530

    Updated

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ io.on('connection', (socket) => {
     });
 });
 
-module.exports = (req, res) => {
-    server.emit('request', req, res);
-};
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
